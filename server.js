e59acf055eb090ac7a0ce6189876573fd64f34df
Crystal <crystal.sundara@gmail.com>
2020-09-30 20:13:33 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 30
-        },
-        {
-            title: 'Rent',
-            budget: 350
-        },
-        {
-            title: 'Grocery',
-            budget: 90
-        },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
+    const budget = JSON.parse(fs.readFileSync('myBudget.json', 'utf8'));
     res.json(budget);
 });
 
